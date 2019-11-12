a645f17a752329239a21f358828969cc046b2b92
KazunoriMatsu <niconico.skyhand@gmail.com>
2019-11-12 15:27:58 +0900

    First devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :customers
+  # devise_scope :user do
+  get 'customers/sign_up' => 'devise/registrations#new'
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
