95e166b230306fbc2b3fe10d72e9783ac05cee90
Masashige1005 <ineimasashige@gmail.com>
2019-11-13 17:23:01 +0900

    Unsubscribed Fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'reviews/create'
   devise_for :customers
   root to: 'products#index'
   # devise_scope :user do
   get 'customers/sign_up' => 'devise/registrations#new'
+  get 'customers/destroy' => 'customers#destroy'
 
   resources :products, only: [:show]
-
+  resources :customers, only: [:show, :destroy]
 end
