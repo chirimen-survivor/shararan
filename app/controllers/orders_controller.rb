cff91c4100ccbce79a102d46555511f79e228464
KazunoriMatsu <niconico.skyhand@gmail.com>
2019-11-13 14:30:26 +0900

    edit_devise

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,32 @@
+class OrdersController < ApplicationController
+	def new
+
+	end
+
+
+	def create
+
+	end
+
+
+	def select
+
+	end
+
+
+	def index
+
+	end
+
+
+	def show
+
+	end
+
+
+	def complete
+
+	end
+
+
+end
