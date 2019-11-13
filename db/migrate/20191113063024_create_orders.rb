689b48c439bf49c809ef1c6b6ea76b30f608a926
KazunoriMatsu <niconico.skyhand@gmail.com>
2019-11-13 20:47:40 +0900

    Revert "no"
    This reverts commit 0dc32297d09e75ba54511d61c57c51e1c1bc35ba.

--- db/migrate/20191113063024_create_orders.rb
+++ db/migrate/20191113063024_create_orders.rb
@@ -2,19 +2,19 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
 
-    	t.string :postal_code1, null: false
-    	t.string :postal_code2, null: false
-    	t.integer :prefecture_code, null: false
-    	t.string :city, null: false
-    	t.string :building, null: false
-    	t.integer :total, null: false
-    	t.integer :status, null: false
-    	t.integer :tax_id, null: false
-    	t.integer :product_id, null: false
-    	t.integer :customer_id, null: false
-    	t.integer :payment, null: false
-    	t.integer :postage_id, null: false
-    	t.integer :quantity, null: false
+    	t.string :postal_code1
+    	t.string :postal_code2
+    	t.integer :prefecture_code
+    	t.string :city
+    	t.string :building
+    	t.integer :total
+    	t.integer :status
+    	t.integer :tax_id
+    	t.integer :product_id
+    	t.integer :customer_id
+    	t.integer :payment
+    	t.integer :postage_id
+    	t.integer :quantity
 
       t.timestamps
     end
