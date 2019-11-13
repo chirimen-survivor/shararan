3c9f9cb3d5e879e81492882f126e4d3a95931c92
KazunoriMatsu <niconico.skyhand@gmail.com>
2019-11-13 21:15:27 +0900

    fix_migtation

--- db/migrate/20191112024730_devise_create_customers.rb
+++ db/migrate/20191112024730_devise_create_customers.rb
@@ -6,6 +6,18 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :postal_code1, null: false
+      t.string :postal_code2, null: false
+      t.integer :prefecture_code, null: false
+      t.string :city, null: false
+      t.string :building, null: false
+      t.string :phone_number1, null: false
+      t.string :phone_number2, null: false
+      t.string :phone_number3, null: false
 
       ## Recoverable
       t.string   :reset_password_token
