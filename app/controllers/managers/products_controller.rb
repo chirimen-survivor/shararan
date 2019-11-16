dd77cabfb77b257f56053e005bb1bea56487d8a8
T-Aruga <baideneo@gmail.com>
2019-11-17 01:33:45 +0900

    Create new-page without nested form

--- app/controllers/managers/products_controller.rb
+++ app/controllers/managers/products_controller.rb
@@ -1,14 +1,26 @@
 class Managers::ProductsController < Managers::ApplicationController
-skip_before_action :authenticate_customer!, only: [:index, :show, :new]
+skip_before_action :authenticate_customer!, only: [:index, :show, :new, :create]
 
 
   def new
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to 'show'
+  end
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :image, :release_date, :status, :description, :artist_id, :category_id, :company_id,)
   end
 end
