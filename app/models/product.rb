913c46851ce3f15b2e33d63780dd8c97677240e5
T-Aruga <baideneo@gmail.com>
2019-11-16 16:51:40 +0900

    Modify Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,21 +5,11 @@ class Product < ApplicationRecord
     #バリデーションチェック
     validates :name, presence: true, length: {minimum: 1, maximum: 20}
     validates :price, presence: true, numericality: { only_integer: true }, length: {minimum: 1, maximum: 8}
-    validate  :image_size
     validates :release_date, presence: true
-    validates :description, presence: true, length: {minimum: 1, maximum: 30}
+    validates :description, presence: true, length: {minimum: 1}
     validates :artist_id, presence: true
     validates :category_id, presence: true
     validates :company_id, presence: true
 
     enum status: { 販売停止: 0, 販売中: 1 }
-
-    private
-
-    # アップロードされた画像のサイズをバリデーションする
-    def image_size
-      if image.size > 5.megabytes
-        errors.add(:image, "5MB以下のファイルを指定してください")
-      end
-    end
 end
