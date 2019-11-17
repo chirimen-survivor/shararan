63481ef2076334607adb8da5e497058a30d8c941
momose-t <momose381@gmail.com>
2019-11-17 14:49:29 +0900

    newotheraddress1

--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,24 @@ ActiveRecord::Schema.define(version: 2019_11_16_094054) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "other_addresses", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code1"
+    t.string "postal_code2"
+    t.integer "prefecture_code"
+    t.string "city"
+    t.string "building"
+    t.string "phone_number1"
+    t.string "phone_number2"
+    t.string "phone_number3"
+    t.integer "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price", null: false
@@ -101,11 +119,11 @@ ActiveRecord::Schema.define(version: 2019_11_16_094054) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.float "rating", null: false
-    t.string "title", null: false
-    t.string "customer_id", null: false
-    t.text "body", null: false
-    t.integer "product_id", null: false
+    t.float "rating"
+    t.string "title"
+    t.string "customer_id"
+    t.text "body"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
