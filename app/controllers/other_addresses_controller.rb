aba2b9772daa303880baa3fb729a06acf8fff583
momose-t <momose381@gmail.com>
2019-11-17 15:52:57 +0900

    createaddress

--- app/controllers/other_addresses_controller.rb
+++ app/controllers/other_addresses_controller.rb
@@ -1,6 +1,6 @@
 class OtherAddressesController < ApplicationController
  def index
-  @other_address = OtherAddress.new
+    @other_address = OtherAddress.new
     @other_addresses = OtherAddress.all
  end
 
@@ -33,4 +33,3 @@ class OtherAddressesController < ApplicationController
       params.require(:other_address).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code1,:postal_code2,:prefecture_code,:city,:building,:building,:phone_number1,:phone_number2,:phone_number3)
   end
 end
-
