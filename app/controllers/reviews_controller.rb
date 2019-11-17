6aac44d397f3116f946e686fb403f4c2fe309c74
Masashige1005 <ineimasashige@gmail.com>
2019-11-17 12:24:44 +0900

    parcial

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
   def create
-  end
+  	@product = Product.find(params[:product_id])
+  	@review = Review.new(review_params)
+	@review.customer_id = current_customer.id
+	@review.product_id = @product.id
+	if @review.save
+		@reviews = @product.reviews.page(params[:page])
+		redirect_to product_path(@product.id)
+	else
+		@reviews = @product.reviews.page(params[:page])
+		render "products/show"
+	end
+ end
+
+private
+	def review_params
+		params.require(:review).permit(:rating, :title, :body, :customer_id, :product_id)
+	end
 end
