c59497cf40c1eb82b04283795242c8e608ea73a9
KazunoriMatsu <niconico.skyhand@gmail.com>
2019-11-17 14:05:23 +0900

    comment out

--- config/routes.rb
+++ config/routes.rb
@@ -24,19 +24,14 @@ Rails.application.routes.draw do
   	resource :reviews, only: [:create]
   end
 
-  # 購入用のルーティング
-  # resources :orders, only: [:new, :index, :show] do
-  # 	get 'create', on: :member
-
-  # 	get 'select', on: :member
-
-  # 	get 'complete', on: :member
-  # end
-
 
   resources :customers, only: [:show, :update, :destroy] do
   	get 'exit', on: :member
+
+  	# レビュー用のルーティング
   	resource :reviews, only: [:create]
+
+  	# 購入用のルーティング
   	resources :orders, only: [:new, :index, :show, :create] do
   		get 'select', on: :member
   		get 'complete', on: :member
