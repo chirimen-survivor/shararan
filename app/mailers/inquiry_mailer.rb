1fec76f15f3e42b3fb48425952bf05389d66273d
momose-t <momose381@gmail.com>
2019-11-18 19:19:49 +0900

    pre mailer

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,2 +1,10 @@
 class InquiryMailer < ApplicationMailer
+	def send_mail
+		@inquiry = inquiry
+		mail(
+			from: 'manager@example.com',
+			top: 'system@example.com',
+			subject: 'しゃららんへのお問い合わせについて'
+			)
+	end
 end
