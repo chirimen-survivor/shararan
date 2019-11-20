83d5ab234b96a004f51152409b24805cf9aa562e
T-Aruga <baideneo@gmail.com>
2019-11-20 16:54:36 +0900

    Add cart action

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,10 +1,19 @@
 class CartItemsController < ApplicationController
-  before_action :correct_customer, only: [:index, :create, :destory]
+  before_action :correct_customer, only: [:index, :destory]
 
   def index
-    @customer = Customer.find(params[:customer_id]) # beforeアクションで定義しているのでなくても大丈夫だが、わかりやすくするため
+    @customer = Customer.find(params[:customer_id]) # beforeアクションで定義しているのでなくても大丈夫だが、コードをわかりやすくするため
     @cart_items = CartItem.where(customer_id: @customer.id)
   end
+
+  # def create
+  #   @product = Product.find(params[:product_id])
+  #   @cart_item = CartItem.new(cart_params)
+  #   @cart_item.customer_id = current_customer.id
+  #   @cart_item.product_id = @product.id
+  #   @cart_item.save!
+  #   redirect_to customer_cart_items_path(@customer.id)
+  # end
 
   private
 
@@ -15,4 +24,8 @@ class CartItemsController < ApplicationController
         redirect_to root_path
       end
     end
+
+    # def cart_params
+    #   params.require(:cart_item).permit(:product_id, :quantity)
+    # end
 end
