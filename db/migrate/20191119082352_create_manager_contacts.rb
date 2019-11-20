d706831ca875ca910733794aa031c8c9652e4637
momose-t <momose381@gmail.com>
2019-11-20 19:24:06 +0900

    delete migration file

--- db/migrate/20191119082352_create_manager_contacts.rb
+++
@@ -1,8 +0,0 @@
-class CreateManagerContacts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :manager_contacts do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_082352) do
+ActiveRecord::Schema.define(version: 2019_11_18_052800) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name", null: false
@@ -68,11 +68,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_082352) do
   create_table "discs", force: :cascade do |t|
     t.integer "product_id", null: false
     t.integer "sequence", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "manager_contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
