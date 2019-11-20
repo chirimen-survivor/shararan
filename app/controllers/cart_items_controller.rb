8b9f7653dd555b4a00a7f50aa32eaf0c7372658e
T-Aruga <baideneo@gmail.com>
2019-11-20 14:01:10 +0900

    Add cart#index

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,18 @@
 class CartItemsController < ApplicationController
-  skip_before_action :authenticate_customer!, only: [:index, :create, :destroy] # 後で消す
+  before_action :correct_customer, only: [:index, :create, :destory]
 
   def index
+    @customer = Customer.find(params[:customer_id]) # beforeアクションで定義しているのでなくても大丈夫だが、わかりやすくするため
+    @cart_items = CartItem.where(customer_id: @customer.id)
   end
+
+  private
+
+    # 正しいユーザーかどうか確認
+    def correct_customer
+      @customer = Customer.find(params[:customer_id])
+      if current_customer.id != @customer.id
+        redirect_to root_path
+      end
+    end
 end
