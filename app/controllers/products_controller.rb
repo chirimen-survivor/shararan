580986427098af3669f78ade9a6a2e4ec4b1199e
momose-t <momose381@gmail.com>
2019-11-21 18:10:09 +0900

    arrival index.html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,10 +33,15 @@ class ProductsController < ApplicationController
     @q = Product.search(search_params)
     @products = @q.result(distinct: true).page(params[:page])
   end
+# 商品編集ページ
+  # def edit
+  #   @product = Product.find(params[:id])
+  # end
 
-  def edit
-    # @cart_item = CartItem.find()
-  end
+  # def update
+    
+  # end
+  # # 
 
   private
 
