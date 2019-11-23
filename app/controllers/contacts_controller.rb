d0ee5a5f327466b50aeb0d311130fef972b06c52
momose-t <momose381@gmail.com>
2019-11-23 19:05:25 +0900

    varidate errar message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,6 +9,7 @@ class ContactsController < ApplicationController
 		@contact = Contact.new(contact_params)
 		@contact.customer_id = current_customer.id
    		if @contact.save
+   			flash[:notice]
     		redirect_to customer_contact_path(@customer.id, @contact)
     	else
     		render :show
